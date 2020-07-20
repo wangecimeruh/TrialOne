6857dd79b9a2cddc54f93757741ced2ab9753d98
wangecimeruh <40371297+wangecimeruh@users.noreply.github.com>
2020-07-20 11:15:58 +0300

    Spring Example

---
+++ SpringExample/src/main/java/tracom.training.quickstart/App.java/App.java
@@ -0,0 +1,11 @@
+package tracom.training.quickstart;
+/*
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
